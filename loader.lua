bd039e998fcd29c0443067085952bff43c543b38
Vortex <121569593+VortexLinx@users.noreply.github.com>
2023-12-23 19:34:31 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,10 @@
 --//Variables
 local games = {
-	[7346416636] = "https://raw.githubusercontent.com/VortexLinx/Hub/main/dontsee/dontseelastchance.lua",
-	[7903991471] = "https://raw.githubusercontent.com/VortexLinx/Hub/main/dontsee/dontseelastchance.lua",
-  [8216162783] = "https://raw.githubusercontent.com/VortexLinx/Hub/main/dontsee/dontseelastchance.lua"
-	
+    [7346416636] = "https://raw.githubusercontent.com/VortexLinx/Hub/main/dontsee/dontseelastchance.lua",
+    [7903991471] = "https://raw.githubusercontent.com/VortexLinx/Hub/main/dontsee/dontseelastchance.lua",
+    [8216162783] = "https://raw.githubusercontent.com/VortexLinx/Hub/main/dontsee/dontseelastchance.lua"
 }
+
 --//Main Code
 if _G.VortexHub ~= nil then
   error("Already executed!")
